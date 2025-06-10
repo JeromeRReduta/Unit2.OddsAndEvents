a5464e875de21a8cd22965cf0a5f1a2c2c461df6
JRReduta <jrreduta@yahoo.com>
2025-06-09 18:42:13 -0700

    finished basic js functionality

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ function main() {
   const app = document.getElementById("app");
   const numStorage = new NumStorage();
   const html = new HTMLData(numStorage);
+  numStorage.add(5);
+  numStorage.add(27);
+  numStorage.sort1();
   html.appendTo(app);
+  numStorage.add(-4);
+  numStorage.sortAll();
+  console.log(numStorage.toString());
 }
 
 main();
